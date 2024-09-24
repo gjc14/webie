03a2fa7a4112c2c7e03a398f2a777480df374228
gjc14 <yin@yinc.me>
2024-09-23 20:21:55 -0500

    feat: webie cms seo crud

---
+++ app/routes/_webie.admin.seo.$tId.delete/route.tsx
@@ -0,0 +1,25 @@
+import { ActionFunctionArgs, json } from '@remix-run/node'
+import { decodedAdminToken } from '~/lib/db/auth.server'
+import { deleteSEO } from '~/lib/db/seo.server'
+
+export const action = async ({ request, params }: ActionFunctionArgs) => {
+	if (request.method !== 'DELETE') {
+		return json({ err: 'Method not allowed' }, { status: 405 })
+	}
+
+	await decodedAdminToken(request.headers.get('Cookie'))
+
+	const id = params.seoId
+
+	if (!id) {
+		return json({ err: `Invalid arguments` }, { status: 400 })
+	}
+
+	try {
+		const { seo } = await deleteSEO(id)
+		return json({ msg: `SEO for ${seo.route || seo.title || 'unknown'} delete` })
+	} catch (error) {
+		console.error(error)
+		return json({ err: 'Failed to delete SEO' }, { status: 500 })
+	}
+}
