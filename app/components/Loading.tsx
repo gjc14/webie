5aa6341447d8bd99d72e6e42a5688b904c83d061
gjc14 <yin@yinc.me>
2024-09-23 20:21:55 -0500

    feat: turnstile; CTA; UnderConstruction; Loading

---
+++ app/components/Loading.tsx
@@ -0,0 +1,26 @@
+import { Loader2 } from 'lucide-react'
+import { cn } from '~/lib/utils'
+
+export const Loading = ({ className, size = 16 }: { className?: string; size?: number }) => {
+	return <Loader2 size={size} className={cn('animate-spin', className)} />
+}
+
+export const FullScreenLoading = () => {
+	return (
+		<div className="z-[9999] fixed inset-0 flex justify-center items-center backdrop-blur-md">
+			<SymmetrySpinner />
+		</div>
+	)
+}
+
+export const SymmetrySpinner = ({ className, white }: { className?: string; white?: boolean }) => {
+	return (
+		<div
+			className={cn(
+				'w-16 h-16 z-50 border-4 border-y-primary border-x-transparent rounded-full animate-spin ease-in-out',
+				white && 'border-y-primary-foreground',
+				className
+			)}
+		></div>
+	)
+}
