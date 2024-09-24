35b16de1597f81743427c5a59fe15ec034662318
gjc14 <yin@yinc.me>
2024-09-23 20:21:55 -0500

    feat: nav and footer; logos; splat * route fix: tailwind config

---
+++ app/components/web/Footer.tsx
@@ -0,0 +1,9 @@
+import { Link } from '@remix-run/react'
+
+export const Footer = () => {
+	return (
+		<footer className="w-full py-3 px-6 mt-16 flex flex-col-reverse items-center justify-center gap-2 lg:flex-row lg:gap-8 border-t">
+			<p className="text-sm text-primary">Built somewhere on the 🌏. © Webie 2024</p>
+		</footer>
+	)
+}
