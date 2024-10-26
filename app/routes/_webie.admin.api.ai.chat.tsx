900e52795d2fcb5f017c2066e25e3eac14e806ff
gjc14 <yin@yinc.me>
2024-10-26 01:01:43 -0500

    feat: claude ai model

--- app/routes/_webie.admin.api.ai.chat.tsx
+++ app/routes/_webie.admin.api.ai.chat.tsx
@@ -1,7 +1,8 @@
+import { anthropic } from '@ai-sdk/anthropic'
 import { google } from '@ai-sdk/google'
 import { openai } from '@ai-sdk/openai'
 import { type ActionFunctionArgs } from '@remix-run/node'
-import { CoreMessage, streamText } from 'ai'
+import { CoreMessage, CoreTool, streamText, StreamTextResult } from 'ai'
 import { userIs } from '~/lib/db/auth.server'
 
 export const googleModels = [
@@ -14,8 +15,13 @@ export const openaiModels = [
     'gpt-4-turbo',
     'gpt-3.5-turbo',
 ] as const
+export const anthropicModels = ['claude-3-5-sonnet-latest'] as const
 
-export const providers = [...googleModels, ...openaiModels] as const
+export const providers = [
+    ...googleModels,
+    ...openaiModels,
+    ...anthropicModels,
+] as const
 
 export type Providers = typeof providers
 export type Provider = Providers[number]
@@ -39,43 +45,53 @@ export async function action({ request }: ActionFunctionArgs) {
         }: { prompt?: string; messages?: CoreMessage[] } & ChatAPICustomBody =
             await request.json()
 
+        let result:
+            | StreamTextResult<Record<string, CoreTool<any, any>>>
+            | undefined = undefined
+
         if (googleModels.includes(provider as (typeof googleModels)[number])) {
-            const result = await streamText({
+            result = await streamText({
                 model: google(provider),
                 system: 'You are a helpful assistant.',
                 messages,
                 prompt,
             })
-            const stream = result.toDataStream()
-
-            // Return as a Response with the correct headers
-            return new Response(stream, {
-                status: 200,
-                headers: new Headers({
-                    'Content-Type': 'text/plain; charset=utf-8',
-                    'X-Vercel-AI-Data-Stream': 'v1',
-                }),
-            })
         } else if (
             openaiModels.includes(provider as (typeof openaiModels)[number])
         ) {
-            const result = await streamText({
+            result = await streamText({
                 model: openai(provider),
                 system: 'You are a helpful assistant.',
                 messages,
                 prompt,
             })
-            const stream = result.toDataStream()
-
-            // Return as a Response with the correct headers
-            return new Response(stream, {
-                status: 200,
-                headers: new Headers({
-                    'Content-Type': 'text/plain; charset=utf-8',
-                    'X-Vercel-AI-Data-Stream': 'v1',
-                }),
+        } else if (
+            anthropicModels.includes(
+                provider as (typeof anthropicModels)[number]
+            )
+        ) {
+            result = await streamText({
+                model: anthropic('claude-3-5-sonnet-latest'),
+                system: 'You are a helpful assistant.',
+                messages,
+                prompt,
             })
         }
+
+        if (!result) {
+            throw new Error('Provider not found')
+        }
+
+        const stream = result.toDataStream()
+
+        // Return as a Response with the correct headers
+        return new Response(stream, {
+            status: 200,
+            headers: new Headers({
+                'Content-Type': 'text/plain; charset=utf-8',
+                'X-Vercel-AI-Data-Stream': 'v1',
+            }),
+        })
     } catch (error) {
         console.error('Streaming error:', error)
         return new Response('Streaming failed', { status: 500 })
