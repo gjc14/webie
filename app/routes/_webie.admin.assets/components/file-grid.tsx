bb662875374a592f685760c6c1f7c3e78545c1cc
gjc14 <yin@yinc.me>
2024-10-31 18:09:02 -0500

    feat: progress card styled and collapsable
    fix repeated added the progress card after complete and added progressCard state management

--- app/routes/_webie.admin.assets/components/file-grid.tsx
+++ app/routes/_webie.admin.assets/components/file-grid.tsx
@@ -1,9 +1,20 @@
-import { Check, CloudUploadIcon, CupSoda, X } from 'lucide-react'
+import {
+    Check,
+    ChevronsUpDown,
+    CloudUploadIcon,
+    CupSoda,
+    X,
+} from 'lucide-react'
 import { useCallback, useEffect, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 
 import AnimatedCircularProgressBar from '~/components/ui/animated-circular-progress-bar'
 import { Button } from '~/components/ui/button'
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from '~/components/ui/collapsible'
 import {
     Dialog,
     DialogContent,
@@ -172,8 +183,21 @@ const FileGridMain = ({
                     url: window.location.origin + updatedFileUrl,
                 }
             })
-        setFileState(prev => [...prev, ...fileUploaded])
+        setFileState(prev => {
+            const newFiles = fileUploaded.filter(
+                newFile =>
+                    !prev.some(existingFile => existingFile.id === newFile.id)
+            )
+            return [...prev, ...newFiles]
+        })
     }, [uploadProgress])
+
+    const [hiddenProgressCards, setHiddenProgressCards] = useState<Set<string>>(
+        new Set()
+    )
+    const visibleUploadProgress = Object.values(uploadProgress).filter(
+        ({ id }) => !hiddenProgressCards.has(id)
+    )
 
     return (
         <div
@@ -228,105 +252,135 @@ const FileGridMain = ({
                 </div>
             )}
 
-            {/* Upload progress card */}
-            {Object.values(uploadProgress).length > 0 && (
-                <div className="absolute w-96 max-h-40 right-3 bottom-3 shadow-sm bg-muted border border-border rounded-lg overflow-auto">
-                    {Object.values(uploadProgress).map(
-                        ({ id, progress, status, error }) => {
-                            const file = fileUploading.find(f => f.id === id)
-                            if (!file) return null
+            {visibleUploadProgress.length > 0 && (
+                <Collapsible className="absolute right-3 bottom-3 w-[350px] space-y-2">
+                    {/* Upload progress card */}
+                    <CollapsibleTrigger asChild>
+                        <Button
+                            variant="ghost"
+                            size="sm"
+                            className="w-full flex items-center justify-between space-x-4 py-0 px-4 border bg-muted"
+                            onClick={e => e.stopPropagation()}
+                        >
+                            <h4 className="text-sm font-semibold">
+                                {visibleUploadProgress.length}{' '}
+                                {visibleUploadProgress.length > 1
+                                    ? 'files'
+                                    : 'file'}{' '}
+                                added
+                            </h4>
+                            <ChevronsUpDown className="h-4 w-4" />
+                            <span className="sr-only">Toggle</span>
+                        </Button>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent className="space-y-2">
+                        {visibleUploadProgress.map(
+                            ({ id, progress, status, error }) => {
+                                const file = fileUploading.find(
+                                    f => f.id === id
+                                )
+                                if (!file) return null
 
-                            return (
-                                <div
-                                    key={id}
-                                    className="flex items-center px-5 py-3.5"
-                                >
-                                    <div className="flex-grow max-w-[calc(100%-2rem)] gap-0.5">
-                                        {/* File name */}
-                                        <div
-                                            className="flex items-center justify-start px-0.5 gap-0"
-                                            title={`${file}`}
-                                        >
-                                            <span className="text-sm font-medium truncate mr-1">
-                                                {file?.name.split('.')[0]}
-                                            </span>
-                                            <span className="text-sm font-medium flex-shrink-0">
-                                                .{file?.name.split('.')[1]}
-                                            </span>
+                                return (
+                                    <div
+                                        key={id}
+                                        className="flex items-center px-5 py-3.5 border rounded-md bg-muted"
+                                    >
+                                        <div className="flex-grow max-w-[calc(100%-2rem)] gap-0.5">
+                                            {/* File name */}
+                                            <div
+                                                className="flex items-center justify-start px-0.5 gap-0"
+                                                title={`${file}`}
+                                            >
+                                                <span className="text-sm font-medium truncate mr-1">
+                                                    {file?.name.split('.')[0]}
+                                                </span>
+                                                <span className="text-sm font-medium flex-shrink-0">
+                                                    .{file?.name.split('.')[1]}
+                                                </span>
+                                            </div>
+
+                                            {/* Error message */}
+                                            {error && (
+                                                <p className="text-sm font-medium text-red-500 dark:text-red-600">
+                                                    {error}
+                                                </p>
+                                            )}
                                         </div>
 
-                                        {/* Error message */}
-                                        {error && (
-                                            <p className="text-sm font-medium text-red-500 dark:text-red-600">
-                                                {error}
-                                            </p>
-                                        )}
-                                    </div>
-
-                                    <div className="ml-3 mr-1 flex flex-shrink-0 items-center">
-                                        {status === 'completed' ? (
-                                            <Button
-                                                variant="outline"
-                                                size="icon"
-                                                className="w-5 h-5 rounded-full bg-lime-500 dark:bg-lime-600 hover:bg-transparent dark:hover:bg-transparent hover:border border-0 border-primary transition-colors duration-200 group"
-                                                onClick={e => {
-                                                    e.stopPropagation()
-                                                    setFileUploading(prev =>
-                                                        prev.filter(
-                                                            file =>
-                                                                file.id !== id
+                                        <div className="ml-3 mr-1 flex flex-shrink-0 items-center">
+                                            {status === 'completed' ? (
+                                                <Button
+                                                    variant="outline"
+                                                    size="icon"
+                                                    className="w-5 h-5 rounded-full bg-lime-500 dark:bg-lime-600 hover:bg-transparent dark:hover:bg-transparent hover:border border-0 border-primary transition-colors duration-200 group"
+                                                    onClick={e => {
+                                                        e.stopPropagation()
+                                                        setHiddenProgressCards(
+                                                            prev => {
+                                                                const newSet =
+                                                                    new Set(
+                                                                        prev
+                                                                    )
+                                                                newSet.add(id)
+                                                                return newSet
+                                                            }
                                                         )
-                                                    )
-                                                }}
-                                            >
-                                                <Check className="h-4 w-4 group-hover:hidden" />
-                                                <X className="h-4 w-4 hidden group-hover:block" />
-                                                <span className="sr-only">
-                                                    {status === 'completed'
-                                                        ? 'Mark as incomplete'
-                                                        : 'Mark as complete'}
-                                                </span>
-                                            </Button>
-                                        ) : status === 'error' ? (
-                                            <Button
-                                                variant="outline"
-                                                size="icon"
-                                                className="w-5 h-5 rounded-full bg-red-500 dark:bg-red-600 hover:bg-transparent dark:hover:bg-transparent hover:border border-0 border-primary transition-colors duration-200 group"
-                                                onClick={e => {
-                                                    e.stopPropagation()
-                                                    setFileUploading(prev =>
-                                                        prev.filter(
-                                                            file =>
-                                                                file.id !== id
+                                                    }}
+                                                >
+                                                    <Check className="h-4 w-4 group-hover:hidden" />
+                                                    <X className="h-4 w-4 hidden group-hover:block" />
+                                                    <span className="sr-only">
+                                                        {status === 'completed'
+                                                            ? 'Mark as incomplete'
+                                                            : 'Mark as complete'}
+                                                    </span>
+                                                </Button>
+                                            ) : status === 'error' ? (
+                                                <Button
+                                                    variant="outline"
+                                                    size="icon"
+                                                    className="w-5 h-5 rounded-full bg-red-500 dark:bg-red-600 hover:bg-transparent dark:hover:bg-transparent hover:border border-0 border-primary transition-colors duration-200 group"
+                                                    onClick={e => {
+                                                        e.stopPropagation()
+                                                        setHiddenProgressCards(
+                                                            prev => {
+                                                                const newSet =
+                                                                    new Set(
+                                                                        prev
+                                                                    )
+                                                                newSet.add(id)
+                                                                return newSet
+                                                            }
                                                         )
-                                                    )
-                                                }}
-                                            >
-                                                <X className="h-4 w-4" />
-                                                <span className="sr-only">
-                                                    {status}
-                                                </span>
-                                            </Button>
-                                        ) : (
-                                            <div className="w-5 h-5 rounded-full border-2 border-muted relative">
-                                                <AnimatedCircularProgressBar
-                                                    className="w-full h-full flex items-center justify-center text-xs animate-pulse"
-                                                    max={100}
-                                                    min={0}
-                                                    value={progress}
-                                                    showPercent={false}
-                                                    strokeWidth={15}
-                                                    gaugePrimaryColor="rgb(79 70 229)"
-                                                    gaugeSecondaryColor="rgba(0, 0, 0, 0.1)"
-                                                />
-                                            </div>
-                                        )}
+                                                    }}
+                                                >
+                                                    <X className="h-4 w-4" />
+                                                    <span className="sr-only">
+                                                        {status}
+                                                    </span>
+                                                </Button>
+                                            ) : (
+                                                <div className="w-5 h-5 rounded-full border-2 border-muted relative">
+                                                    <AnimatedCircularProgressBar
+                                                        className="w-full h-full flex items-center justify-center text-xs animate-pulse"
+                                                        max={105}
+                                                        min={0}
+                                                        value={progress}
+                                                        showPercent={false}
+                                                        strokeWidth={15}
+                                                        gaugePrimaryColor="rgb(79 70 229)"
+                                                        gaugeSecondaryColor="rgba(0, 0, 0, 0.1)"
+                                                    />
+                                                </div>
+                                            )}
+                                        </div>
                                     </div>
-                                </div>
-                            )
-                        }
-                    )}
-                </div>
+                                )
+                            }
+                        )}
+                    </CollapsibleContent>
+                </Collapsible>
             )}
         </div>
     )
